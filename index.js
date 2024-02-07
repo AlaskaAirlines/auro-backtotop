af85cef6caa555d4823c3069ee3944c204460115
Jason Baker <jason@capsule42.com>
2024-02-07 11:12:51 -0600

    fix(import): correct path for file import #25

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import { AuroBackToTop } from './src/auro-backtotop.js';
+import { AuroBackToTop } from './dist/auro-backtotop.js';
 
 /**
  * Register Custom Element.
  * @param {Object} name - Name to use for custom element.
  * @returns {void}
  */
- const registerComponent = (name = 'custom-backtotop') => {
+const registerComponent = (name = 'custom-backtotop') => {
   // alias definition
   if (!customElements.get(name)) {
     customElements.define(name, class extends AuroBackToTop {});
   }
-}
+};
 
-export { registerComponent }
+export { registerComponent };
